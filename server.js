7cf761894aaf52d753afca186b89d3adb2a8e350
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-10-07 15:25:27 -0500

    MVP with server updated

--- server.js
+++ server.js
@@ -2,48 +2,60 @@ const express = require('express');
 
 const db = require('./data/dbConfig.js');
 
+
 const server = express();
 
 server.use(express.json());
-server.get('/', (req, res) => {
-    db('accounts').select('*')
-    .then(accounts => {
-        res.status(200).json(accounts)
-    }).catch(err => {
-        res.json(err)
-    })
+
+server.get('/api/accounts', (req, res) => {
+    db('accounts')
+        .then(accounts => {
+            res.status(200).json(accounts)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'there was an issue getting the accounts from the database'})
+        })
 })
 
- server.get('/:id', (req, res) => {
-    // select * from posts where id = 2
-    const {id} = req.params
-    db('accounts').where({id}) //always returns an array
-    .first() //picks the first element of the resulting array
-    .then(account => {
-        res.status(200).json(account)
-    })
-    .catch(err => {
-        res.json(err)
-    })
-
- });
-
- server.post('/', (req, res) => {
-    // insert into posts (fields) values ()
-    const accountsData = req.body
-    // validate the data before inserting into db
-
-     db('accounts').insert(accountsData, 'id')
-    .then(([id]) => {
-        db('accounts').where({id}) //always returns an array
-        .first() //picks the first element of the resulting array
+server.get('/api/accounts/:id', (req, res) => {
+    db('accounts').where({id: req.params.id})
         .then(account => {
             res.status(200).json(account)
         })
-    })
-    .catch(err => {
-        res.json(err)
-    })
-});
+        .catch(err => {
+            res.status(500).json({error: 'there was an issue getting the account from the database/ it does not exist'})
+        })
+})
+
+server.post('/api/accounts', (req, res) => {
+    db('accounts').insert({name: req.body.name, budget: req.body.budget})
+        .then(account => {
+            res.status(201).json(account)
+
+        })
+        .catch(err => {
+            res.status(500).json({error: 'there was an issue createing the account in the database'})
+        })
+})
+
+server.put('/api/accounts/:id', (req, res) => {
+    db('accounts').where({id: req.params.id}).update({name: req.body.name, budget: req.body.budget})
+        .then(num => {
+            res.status(200).json(num)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'there was an issue updateing the record in the database'})
+        })
+}) 
+
+server.delete("/api/accounts/:id", (req, res) => {
+    db('accounts').where({id: req.params.id}).del()
+        .then(num => {
+            res.status(200).json(num)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'there was an issue deleteing the record from the server'})
+        })
+})
 
 module.exports = server;
